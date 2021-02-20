1ab676eb2ea096bea646bae63dc5255bff41856c
Chris Ritter <chrisritter2@gmail.com>
2021-02-20 14:12:57 -0500

    Finished scheduler.cpp and started on stats.cpp

--- scheduler/scheduler.cpp
+++ scheduler/scheduler.cpp
@@ -4,11 +4,40 @@
  *  Created on: Sep 8, 2019
  *      Author: keith
  */
-
+#include <queue>
+#include "../includes/scheduler.h"
+#include "../includes/PCB.h"
 //TODO fill in content
 
+//add a process, either a new one or one that
+//had been running on the CPU and has been preempted
+void Scheduler::add(PCB p) {
+	ready_q->push(p);
+	sort();
+}
 
+//get next process
+PCB Scheduler::getNext() {
+	PCB first = ready_q->front();
+	ready_q->pop();
+	return first;
+}
 
+//returns true if there are no  jobs in the readyQ
+//false otherwise
+bool Scheduler::isEmpty() {
+	return ready_q->empty();
+}
 
-
-
+//if process has completed (used all its remaining_cpu_time) or
+//if we are using a preemptive scheduling algorithm and the
+//timeslice is over then its time to switch processes
+//returns:
+//true - switch processes
+//false - do not switch
+bool Scheduler::time_to_switch_processes(int tick_count, PCB &p) {
+	if (p.remaining_cpu_time <= 0 || (preemptive && tick_count >= time_slice)) {
+		return true;
+	}
+	return false;
+}
